49458da62766df0c4ff8c94b3d3cf089a60be90e
28gillelsr1 <79170238+28gillelsr1@users.noreply.github.com>
2021-11-30 18:43:28 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function clickHere() {
+    window.location="types.html";
+    
+}
+
+function back(){
+    window.location="types.html";
+}
---
+++ types.js
@@ -0,0 +1,21 @@
+function mammals(){
+    window.location="mammal.html";
+}
+function birds(){
+    window.location="birds.html";
+}
+function reptiles(){
+    window.location="reptiles.html";
+}
+function amphibians(){
+    window.location="amphibians.html";
+}
+function fish(){
+    window.location="fish.html";
+}
+function invertebrate(){
+    window.location="invertebrate.html";
+}
+function back(){
+    window.location="index.html";
+}
